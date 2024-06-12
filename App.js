0a6efc6b72f25fa8c0213d24137dde3d0be30496
Diivvuu <divyanshubawa9@gmail.com>
2024-06-12 15:29:14 +0530

    all folder structure adjusted

---
+++ App.js
@@ -0,0 +1,17 @@
+// import logo from "./logo.svg";
+import "./App.css";
+import "./index.css";
+// import Login from "./components/Login";
+// import SignUp from "./components/SignUp";
+// import Navbar from "./components/Navbar";
+import Dashboard from "./components/Dashboard";
+function App() {
+  return (
+    <div className="App">
+      {/* <Navbar /> */}
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
